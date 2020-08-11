45addb9d3421d1bf04755cb3cee5d7f59bacae61
phantomcosmonaut <bugbeeb@gmail.com>
2020-08-11 15:48:54 -0400

    modify docker command

--- docker-cmd.sh
+++ docker-cmd.sh
@@ -1,7 +1,4 @@
 #!bin/bash
-echo "pulling from github"
-cd /home/ec2-user/angular-gitapi
-git pull phantomcosmonaut/angular-gitapi
-wait
-echo "running webpack"
+
+echo "starting angular-githubapi"
 docker start angular-githubapi
