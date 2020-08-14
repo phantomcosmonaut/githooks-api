fdaf10391c950dbbd7c5cead65ccee0d3cd8e5bd
phantomcosmonaut <bugbeeb@gmail.com>
2020-08-13 20:31:39 -0400

    quick update

--- installation.sh
+++ installation.sh
@@ -7,4 +7,3 @@ apt install git
 git clone https://github.com/phantomcosmonaut/angular-gitapi.git
 pip install -r ./requirements.txt
 chmod o+x ./angular-gitapi/
-cd ./angular-gitapi/
--- ngbuild.sh
+++ ngbuild.sh
@@ -1,4 +1,6 @@
 echo "starting angular-githubapi"
+cd ./angular-gitapi/
 git pull
 npm install
 npm run ng build
+cd /
