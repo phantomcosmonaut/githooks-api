5560a411aa0f716d150e5e251c7b92e04481dc32
phantomcosmonaut <bugbeeb@gmail.com>
2020-08-14 11:42:12 -0400

    fixes

--- installation.sh
+++ installation.sh
@@ -1,9 +1,11 @@
 apt-get update
+pip install --upgrade pip
+npm update -g
 echo "node version: " 
 node --version
 echo "python verion: " 
 python --version
+pip install -r ./requirements.txt
 apt install git
 git clone https://github.com/phantomcosmonaut/angular-gitapi.git
-pip install -r ./requirements.txt
 chmod o+x ./angular-gitapi/
