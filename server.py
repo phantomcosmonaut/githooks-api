153a295246d62d1191e4f1a870659b5e97c1e13e
phantomcosmonaut <bugbeeb@gmail.com>
2020-08-18 08:59:10 -0400

    server changes

--- server.py
+++ server.py
@@ -4,17 +4,18 @@ import subprocess
 cmd = ["./ngpull.sh"]
 
 app = Flask(__name__)
-@app.route('/', methods=['POST'])
-def Post():
-    process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, stderr = process.communicate()
-    stdout = stdout.decode("utf-8")
-    stderr = stderr.decode("utf-8")
-    message = {"Output": stdout, "Error": stderr}
-    if len(stderr) > 0:
-        return message, 500
-    else:
-        return message, 200
+@app.route('/', methods=['POST', 'GET'])
+def Handler():
+    if request.method == 'GET':
+        return {"Message": "Server is up and running on port 6000"}, 200
+    else if request.method == 'POST':
+        process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+        stdout, stderr = process.communicate()
+        message = {"Output": stdout, "Error": stderr}
+        if len(stderr) > 0:
+            return message, 500
+        else:
+            return message, 200
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=6000)
