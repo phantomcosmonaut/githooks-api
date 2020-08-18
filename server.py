7335e6d230a66e8ad13f6d5b78482b4600079d79
phantomcosmonaut <bugbeeb@gmail.com>
2020-08-17 20:38:19 -0400

    modify subprocess

--- server.py
+++ server.py
@@ -1,13 +1,20 @@
-from flask import Flask, jsonify
+from flask import Flask
 import subprocess
 
-cmd = ["ngpull.sh"]
+cmd = ["./ngpull.sh"]
 
 app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def Post():
-    subprocess.Popen(cmd, shell=True, executable="bin/bash")
-    return jsonify({"message": "Request received."})
+    process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    stdout = stdout.decode("utf-8")
+    stderr = stderr.decode("utf-8")
+    message = {"Output": stdout, "Error": stderr}
+    if len(stderr) > 0:
+        return message, 500
+    else:
+        return message, 200
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=6000)
